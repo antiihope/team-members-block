f7d19ae499a550f7c26b63267cbd8ebe74328f90
Hazem <h.hussien@singularity.finance>
2023-11-16 12:46:48 +0200

    Add block transforms for team members

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { registerBlockType } from '@wordpress/blocks';
+import { registerBlockType, createBlock } from '@wordpress/blocks';
 import './team-member';
 import './style.scss';
 import Edit from './edit';
@@ -7,4 +7,49 @@ import save from './save';
 registerBlockType( 'block-course/team-members', {
 	edit: Edit,
 	save,
+	transforms: {
+		from: [
+			{
+				type: 'block',
+				blocks: [ 'core/gallery' ],
+				transform: ( { images, columns } ) => {
+					const innerBlocks = images.map( ( { url, id, alt } ) => {
+						return createBlock( 'block-course/team-member', {
+							url,
+							id,
+							alt,
+						} );
+					} );
+					return createBlock(
+						'block-course/team-members',
+						{
+							columns: columns || 2,
+						},
+						innerBlocks
+					);
+				},
+			},
+			{
+				type: 'block',
+				blocks: [ 'core/image' ],
+				isMultiBlock: true,
+				transform: ( attrs ) => {
+					const innerBlocks = attrs.map( ( { url, id, alt } ) => {
+						return createBlock( 'block-course/team-member', {
+							url,
+							id,
+							alt,
+						} );
+					} );
+					return createBlock(
+						'block-course/team-members',
+						{
+							columns: attrs.length > 3 ? 3 : attrs.length,
+						},
+						innerBlocks
+					);
+				},
+			},
+		],
+	},
 } );
